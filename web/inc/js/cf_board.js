9c694cb6ed61b225522620a49a554af1257e18ef
brettmelton <brettmelton@gmail.com>
2014-10-23 20:42:45 -0700

    Zebra strip survey rows

--- web/inc/js/cf_board.js
+++ web/inc/js/cf_board.js
@@ -1,6 +1,14 @@
 
 $(document).ready(function() { 
+
   $('.surveyresults tbody tr:even').addClass('AccessListItemsEven');
   $('.surveyresults tbody tr:odd').addClass('AccessListItemsOdd');
+
+  $('.surveyresults tbody tr').hover( function() {
+    $(this).addClass('AccessListItemsHover');
+  }, function() {
+    $(this).removeClass('AccessListItemsHover');
+  });
+
  });
-  
+
